98dfe47f34abb40ccab0469d2dc88504349f4ea4
Patrick WANG <ruochwang@gmail.com>
2020-01-13 15:10:45 +0800

    change code format

--- sample/aggregation.cpp
+++ sample/aggregation.cpp
@@ -37,18 +37,18 @@ bool Verify(const G1 &sign, const G2 &Q, const G2 &pub, const void *m, int size)
     return e1 == e2;
 }
 
-void Aggregation(G1 &result, std::vector <G1> sign_list) {
+void Aggregation(G1 &result, std::vector<G1> sign_list) {
     G1::add(result, sign_list[0], sign_list[1]);
-    for (int i = 2; i < (int)sign_list.size(); i++) {
+    for (int i = 2; i < (int) sign_list.size(); i++) {
         G1::add(result, result, sign_list[i]);
     }
 }
 
 bool
 AggregateVerification(G1 &signs, const G2 &Q, const G2 &pub, std::vector<void *> chunks, std::vector<int> chunk_size) {
-    std::vector <G1> hash_list;
+    std::vector<G1> hash_list;
     G1 Hm;
-    for (int i = 0; i < (int)chunks.size(); i++) {
+    for (int i = 0; i < (int) chunks.size(); i++) {
         Hash(Hm, chunks[i], chunk_size[i]);
         hash_list.push_back(Hm);
     }
@@ -56,14 +56,14 @@ AggregateVerification(G1 &signs, const G2 &Q, const G2 &pub, std::vector<void *>
     Fp12 e_left, e_sum;
     pairing(e_left, signs, Q);
 
-    std::vector <Fp12> e_list;
+    std::vector<Fp12> e_list;
     Fp12 e;
-    for (int i = 0; i < (int)chunks.size(); i++) {
+    for (int i = 0; i < (int) chunks.size(); i++) {
         pairing(e, hash_list[i], pub);
         e_list.push_back(e);
     }
     Fp12::mul(e_sum, e_list[0], e_list[1]);
-    for (int i = 2; i < (int)chunks.size(); i++) {
+    for (int i = 2; i < (int) chunks.size(); i++) {
         Fp12::mul(e_sum, e_list[i], e_sum);
     }
 
@@ -84,8 +84,8 @@ void run_exp2(std::vector<void *> chunks, std::vector<int> chunk_size) {
 
     // sign
     G1 sign;
-    std::vector <G1> sign_list;
-    for (int i = 0; i < (int)chunks.size(); i++) {
+    std::vector<G1> sign_list;
+    for (int i = 0; i < (int) chunks.size(); i++) {
         Sign(sign, s, chunks[i], chunk_size[i]);
         sign_list.push_back(sign);
     }
@@ -108,12 +108,12 @@ int main(int argc, char *argv[]) {
     std::string m2 = "str2 for test";
     std::string m3 = "str 3 for test loops";
 
-    std::vector<void*> chunks;
+    std::vector<void *> chunks;
     std::vector<int> chunk_size;
 
-    chunks.push_back(static_cast<void*> (&m1));
-    chunks.push_back(static_cast<void*> (&m2));
-    chunks.push_back(static_cast<void*> (&m3));
+    chunks.push_back(static_cast<void *> (&m1));
+    chunks.push_back(static_cast<void *> (&m2));
+    chunks.push_back(static_cast<void *> (&m3));
 
     chunk_size.push_back(sizeof(m1));
     chunk_size.push_back(sizeof(m2));
